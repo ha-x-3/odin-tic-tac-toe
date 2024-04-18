028495153e31a9b5ba84bcf95f49364fee309a31
Brittney Epperson <beppers87@hotmail.com>
2024-04-18 12:59:05 -0500

    Added logic for calculating winning game

--- script.js
+++ script.js
@@ -62,8 +62,34 @@ const Game = (() => {
         }
             
         Gameboard.update(index, players[currentPlayerIndex].mark);
+
+        if (checkForWin(Gameboard.getGameBoard(), players[currentPlayerIndex].mark)) {
+            gameOver = true;
+            alert(`${players[currentPlayerIndex].name} won!`);
+        }
+
         currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     };
+
+    function checkForWin(board) {
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+        for (let i = 0; i < winningCombinations.length; i++) {
+            const [a, b, c] = winningCombinations[i];
+            if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     const restart = () => {
         for (let i = 0; i < 9; i++) {
